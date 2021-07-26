8f753bb26d4f2b3bf006c6eac512d0fb00544079
datata <dani@danitarazona.dev>
2021-07-26 23:11:01 +0200

    [ADD] vue router dependency added

---
+++ src/views/CvApp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    This is an cv-app page
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CvApp',
+  components: {
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    ...hello World ^^!
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
