5a8765b5824faf1345fb43e4957c444d43ac9a75
datata <dani@danitarazona.dev>
2021-07-28 00:04:33 +0200

    [ADD] element ui dependecy installed & littel form added

--- src/views/CvApp.vue
+++ src/views/CvApp.vue
@@ -1,14 +1,50 @@
 <template>
   <div>
-    This is an cv-app page
+    <el-form ref="form" :model="form">
+      <el-form-item>
+        <el-input placeholder="Your Name" v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input
+          placeholder="Your profession"
+          v-model="form.profession"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input placeholder="Your email" v-model="form.email"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input placeholder="Your phone" v-model="form.phone"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Create Pdf</el-button>
+      </el-form-item>
+    </el-form>
+
+ 
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'CvApp',
+  name: "CvApp",
   components: {
-  }
-}
+  },
+  data() {
+    return {
+      form: {
+        name: "",
+        profession: "",
+        email: "",
+        phone: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log('onSubmit');
+    },
+  },
+};
 </script>
